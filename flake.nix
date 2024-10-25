2365c68e40dce8ba16e72802cc7895cad18ed2d2
Michael Klug <michael.klug@fullstacks.eu>
2024-10-25 16:15:40 +0200

    fix: working without home manager

--- flake.nix
+++ flake.nix
@@ -5,17 +5,13 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     nix-darwin.url = "github:LnL7/nix-darwin";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
-    home-manager.url = "github:nix-community/home-manager";
-home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
   outputs = inputs@{ self, nix-darwin, nixpkgs }:
   let
     configuration = { pkgs, ... }: {
-      # List packages installed in system profile. To search by name, run:
-      # $ nix-env -qaP | grep wget
       environment.systemPackages =
-        [ 
+        [
           pkgs.yq-go
           pkgs.jq
           pkgs.lens
@@ -58,7 +54,6 @@ home-manager.inputs.nixpkgs.follows = "nixpkgs";
       # Necessary for using flakes on this system.
       nix.settings.experimental-features = "nix-command flakes";
 
-      # Create /etc/zshrc that loads the nix-darwin environment.
       programs.zsh = {
         enable = true;
         enableCompletion = true;
@@ -84,16 +79,22 @@ home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
       # The platform the configuration will be used on.
       nixpkgs.hostPlatform = "aarch64-darwin";
+
+      system.defaults = {
+        dock.autohide = true;
+        dock.mru-spaces = false;
+        finder.AppleShowAllExtensions = true;
+        finder.FXPreferredViewStyle = "clmv"; # does not work
+        loginwindow.LoginwindowText = "FullStacks Oida!";
+      };
     };
   in
   {
-    # Build darwin flake using:
-    # $ darwin-rebuild build --flake .#FullStackS-MacBook-Pro
-    darwinConfigurations."FullStackS-MacBook-Pro" = nix-darwin.lib.darwinSystem {
+    darwinConfigurations."macos" = nix-darwin.lib.darwinSystem {
       modules = [ configuration ];
     };
 
     # Expose the package set, including overlays, for convenience.
-    darwinPackages = self.darwinConfigurations."FullStackS-MacBook-Pro".pkgs;
+    darwinPackages = self.darwinConfigurations."macos".pkgs;
   };
 }
